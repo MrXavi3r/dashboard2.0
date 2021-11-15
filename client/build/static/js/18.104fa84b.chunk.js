15495ad677cf185677bef03bee6a532d1d671f6e
Xavier Ritch <xritch89@hotmail.com>
2021-11-14 20:48:56 -0500

    build folder added for deploy

---
+++ client/build/static/js/18.104fa84b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdashboard=this.webpackJsonpdashboard||[]).push([[18],{380:function(a,e,s){"use strict";s.r(e);var t=s(0),d=s.n(t);e.default=()=>d.a.createElement("div",null,d.a.createElement("div",{className:"page-header"}))}}]);
+//# sourceMappingURL=18.104fa84b.chunk.js.map
